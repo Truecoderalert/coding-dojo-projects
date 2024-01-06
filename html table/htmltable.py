821d5b3329ecc1ae4f97f950df9b9ca1b4be7431
Truecoderalert <141266977+Truecoderalert@users.noreply.github.com>
2024-01-06 17:27:33 -0500

    Add files via upload

---
+++ html table/htmltable.py
@@ -0,0 +1,15 @@
+from flask import Flask , render_template
+app = Flask(__name__)
+
+@app.route('/user')
+def return_user():
+    users = [
+            {'first_name' : 'Michael', 'last_name' : 'Choi'},
+            {'first_name' : 'John', 'last_name' : 'Supsupin'},
+            {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+            {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+    return render_template('user.html' , users = users)
+
+if __name__=="__main__":      
+        app.run(debug=True) 
